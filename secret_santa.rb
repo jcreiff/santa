7859d30c8321df159d7e3cec32dc8c0ee4f9a259
Joe Reiff <jcreiff@gmail.com>
2015-09-16 11:34:55 -0400

    minor refactoring for readability

--- secret_santa.rb
+++ secret_santa.rb
@@ -6,8 +6,9 @@ def santa_matcher(santas) #takes in array from str_to_list
   matches = []
   recipients = list_check(santas)
   santas.each do |name|
-    assigned = matches.map{|match| match[1][2]}
-    options = eliminate_family(recipients, name[1]).delete_if{|option| assigned.include?(option[2])}
+    assigned_emails = matches.map{|match| match[1][2]}
+    options = eliminate_family(recipients, name[1])
+    options.delete_if{|option| assigned_emails & [option[2]] != []}
     matches<<[name, options.sample]
   end
   matches.any?{|match| match[1] == nil} ? santa_matcher(santas) : matches
@@ -21,5 +22,5 @@ def list_check(names) #takes in array from str_to_list
 end
 
 def eliminate_family(names, name) #takes in hash from list_check
-  return names.reject{|k,v| k==name}.values.flatten(1)
+  names.reject{|k,v| k==name}.values.flatten(1)
 end
